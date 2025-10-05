7e9ff6a9c498a0a75f8a828474bdc4358102e7f3
Farzaadshojaei <farzaadshojaei@gmail.com>
2025-10-05 13:55:51 +0330

    Changings in Banner

---
+++ src/components/TestingBanner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TestingBanner.css';
+
+const TestingBanner = () => {
+  return (
+    <div className="testing-banner">
+      <div className="testing-banner-content">
+        <span className="testing-banner-text">
+          🚧 Company is a Stage (Testing) Site 🚧
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default TestingBanner;
